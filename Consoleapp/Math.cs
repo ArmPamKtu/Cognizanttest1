a848f6514fcd1d111a4c253c22770e50d049d577
805345 <805345@LTLT10618>
2019-07-15 15:01:11 +0300

    Created math app, with sum and substraction functions

---
+++ Consoleapp/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consoleapp
+{
+    public class Math
+    {
+        //public static int myConstant = 5;
+        public int Sum(int number1, int number2)
+        {
+            int temp = number1 + number2;
+            return temp;
+        }
+        public int Substract(int number1, int number2)
+        {
+            int temp = number1 - number2;
+            return temp;
+        }
+    }
+}
---
+++ Consoleapp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            var math = new Math();
+            int num1, num2;
+
+            Console.WriteLine($"please enter two numbers");
+
+            input = Console.ReadLine();
+            Int32.TryParse(input, out num1);
+
+            input = Console.ReadLine();
+            Int32.TryParse(input, out num2);
+
+            var sum = math.Sum(num1, num2);
+            Console.WriteLine($"{num1} + {num2} = {sum}");
+
+            var substract = math.Substract(num1, num2);
+            Console.WriteLine($"{num1} + {num2} = {substract}");
+
+            //var myConst = Math.myConstant;
+            //DateTime time = DateTime.Now;
+        }
+    }
+}
